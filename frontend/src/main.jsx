81e96012c680350acac181a693801ace3e0325be
belkysupreme22 <107432127+belkysupreme22@users.noreply.github.com>
2025-11-04 12:03:33 +0300

    fix build error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SpeedInsights />
     <Analytics />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
